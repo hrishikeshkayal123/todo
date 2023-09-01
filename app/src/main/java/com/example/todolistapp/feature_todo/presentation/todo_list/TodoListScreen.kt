8e52eb7125b49269db56e763d679d26e26844ef5
spark-hrishikeshkayal <92143334+spark-hrishikeshkayal@users.noreply.github.com>
2023-09-01 07:25:58 +0530

    Todo: Preparing the TodoList screen

--- app/src/main/java/com/example/todolistapp/feature_todo/presentation/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/example/todolistapp/feature_todo/presentation/todo_list/TodoListScreen.kt
@@ -1,15 +1,43 @@
 package com.example.todolistapp.feature_todo.presentation.todo_list
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.todolistapp.feature_todo.presentation.viewmodel.TodoViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoListScreen(
     navigation: NavController,
     viewModel : TodoViewModel = hiltViewModel()
 ) {
+    val state = viewModel.state.value
+    val scope = rememberCoroutineScope()
+    val snackBarState = remember { SnackbarHostState() }
 
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
 
+                },
+                contentColor = MaterialTheme.colorScheme.background
+            ) {
+                Icon(imageVector= Icons.Default.Add,contentDescription = "add todo")
+            }
+        }
+    ) {
+
+    }
 }
